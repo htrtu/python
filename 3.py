3c594faa14433a688d0e7bc69b7effab0e3dbab7
htrtu <feikomilo1@gmail.com>
2016-04-15 16:23:48 +0300

    3
    Ввести восемь чисел через запятую — целочисленные координаты 4-х попарно несовпадающих точек A1, A2, A3 и A4: X1, Y1, X2, Y2, X3, Y3, X4, Y4. Вывести YES, если прямая A1A2 параллельна прямой A3A4 (или совпадает с ней), и NO — если не параллельна.

---
+++ 3.py
@@ -0,0 +1,10 @@
+print 'Vvedite 8 koordinat cherez zapyatuyu: '
+a = raw_input().split(',')
+print(a)
+line1 = (int(a[3])-int(a[1]))/(int(a[2])-int(a[0]))
+line2 = (int(a[7])-int(a[5]))/(int(a[6])-int(a[4]))
+if line1 == line2:
+    print('YES')
+else:
+    print('NO')
+
